9e0f241f1c4f7520a15a6d2d564705d6e9e19115
alfredo.sanzo@gmail.com <alfredo.sanzo@gmail.com>
2017-02-10 19:41:25 -0500

    Mumuki Export on 2017-02-10 19:41:25 -0500

--- 00074_TEG, La Revancha/test.pl
+++ 00074_TEG, La Revancha/test.pl
@@ -2,4 +2,4 @@ test(el_mas_fuerte):-
   elPaisMasFuerte(canada).
   
 test(el_mas_fuerte_no_son_los_otros):-
-  not((elPaisMasFuerte(Pais), Pais \= canada)).
+  not((elPaisMasFuerte(Pais), Pais \= canada)). 
