ee9fd1fc8d0e700a4e97db5db38b749381531f30
mmatos87@gmail.com <mmatos87@gmail.com>
2018-06-14 18:45:10 -0400

    Mumuki Export on 2018-06-14 18:45:10 -0400

--- 00075_Gran compañero de viaje/test.pl
+++ 00075_Gran compañero de viaje/test.pl
@@ -1,7 +1,5 @@
 test(juancho_es_gran_companiero_para_pepe, nondet):-
-	granCompanieroDeViaje(Participante, Companiero),
-	assertion(Companiero == juancho),
-	assertion(Participante == pepe).
+	granCompanieroDeViaje(pepe, juancho).
  
 test(lucy_no_es_gran_companiera_para_pepe, fail):-
 	granCompanieroDeViaje(pepe, lucy).
